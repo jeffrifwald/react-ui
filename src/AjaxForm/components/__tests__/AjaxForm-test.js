c52d885cb73d8a717bf67800768236ed2165e456
Jeff McRiffey <jeff.mcriffey@ambition.com>
2015-08-10 16:58:46 -0400

    convert AjaxForm tests

--- src/AjaxForm/components/__tests__/AjaxForm-test.js
+++ src/AjaxForm/components/__tests__/AjaxForm-test.js
@@ -1,14 +1,13 @@
-import {assert} from 'chai';
+import Mingus from 'mingus';
 import React from 'react';
-import {stub} from 'sinon';
 
 import AjaxForm from '../AjaxForm';
-import {request, TestUtils} from '../../../utils';
+import {request} from '../../../utils';
 
 
-describe('AjaxForm/AjaxForm', () => {
-    it('should render the correct top level elements', () => {
-        const component = TestUtils.createComponent(
+Mingus.createTestCase('AjaxFormTest', {
+    testRender() {
+        const component = this.createComponent(
             <AjaxForm action="/login/" className="cool-form">
                 <input />
                 <button>Submit</button>
@@ -16,116 +15,105 @@ describe('AjaxForm/AjaxForm', () => {
         );
         const rendered = component.render();
 
-        assert.equal(rendered.type, 'form');
-        assert.equal(rendered.props.children.length, 2);
-        assert.equal(rendered.props.action, '/login/');
-        assert.equal(rendered.props.method, 'POST');
-        assert.equal(rendered.props.children[0].type, 'input');
-        assert.equal(rendered.props.children[1].type, 'button');
-        assert.equal(rendered.props.onSubmit, component.onSubmit);
-        assert.equal(
-            rendered.props.className,
-            'react-ui-ajax-form cool-form'
-        );
-    });
+        this.assertIsType(rendered, 'form');
+        this.assertHasClass(rendered, 'react-ui-ajax-form cool-form');
+        this.assertEqual(rendered.props.action, '/login/');
+        this.assertEqual(rendered.props.method, 'POST');
+        this.assertEqual(rendered.props.onSubmit, component.onSubmit);
+        this.assertNumChildren(rendered, 2);
+        this.assertNthChildIsType(rendered, 0, 'input');
+        this.assertNthChildIsType(rendered, 1, 'button');
+    },
 
-    it('should accept url instead of action', () => {
-        const component = TestUtils.createComponent(
+    testUrlInsteadOfAction() {
+        const rendered = this.renderComponent(
             <AjaxForm url="/login/">
                 <input />
                 <button>Submit</button>
             </AjaxForm>
         );
-        const rendered = component.render();
 
-        assert.equal(rendered.props.action, '/login/');
-    });
+        this.assertEqual(rendered.props.action, '/login/');
+    },
 
-    it('should handle onResponse', () => {
-        const onResponse = stub();
-        const component = TestUtils.createComponent(
+    testOnResponse() {
+        const onResponse = this.stub();
+        const component = this.createComponent(
             <AjaxForm onResponse={onResponse} />
         );
 
         component.onResponse('mock error', 'mock response');
-        assert.equal(onResponse.callCount, 1);
-        assert.isTrue(onResponse.calledWith('mock error', 'mock response'));
-    });
+        this.assertEqual(onResponse.callCount, 1);
+        this.assertTrue(onResponse.calledWith('mock error', 'mock response'));
+    },
 
-    it('should handle a onSubmit', () => {
-        const onSubmit = stub();
-        const evt = {preventDefault: stub()};
-        const component = TestUtils.createComponent(
+    testOnSubmit() {
+        const onSubmit = this.stub();
+        const mockEvt = {preventDefault: this.stub()};
+        const component = this.createComponent(
             <AjaxForm onSubmit={onSubmit} />
         );
 
-        stub(component, 'submit');
+        this.stub(component, 'submit');
 
-        component.onSubmit(evt);
-        assert.equal(evt.preventDefault.callCount, 1);
-        assert.equal(onSubmit.callCount, 1);
-        assert.equal(component.submit.callCount, 1);
-        assert.isTrue(onSubmit.calledWith(evt));
+        component.onSubmit(mockEvt);
+        this.assertEqual(mockEvt.preventDefault.callCount, 1);
+        this.assertEqual(onSubmit.callCount, 1);
+        this.assertEqual(component.submit.callCount, 1);
+        this.assertTrue(onSubmit.calledWith(mockEvt));
+    },
 
-        component.submit.restore();
-    });
-
-    it('should submit with FormData', () => {
-        const component = TestUtils.createComponent(
+    testSubmitWithFormData() {
+        const component = this.createComponent(
             <AjaxForm />
         );
 
-        global.FormData = true;
         component.refs = {form: 'mock form'};
-        stub(React, 'findDOMNode', () => 'mock node');
-        stub(component, 'submitFormData');
+        global.FormData = true;
+        this.stub(React, 'findDOMNode', () => 'mock node');
+        this.stub(component, 'submitFormData');
 
         component.submit();
-        assert.equal(React.findDOMNode.callCount, 1);
-        assert.equal(component.submitFormData.callCount, 1);
-        assert.isTrue(React.findDOMNode.calledWith('mock form'));
-        assert.isTrue(component.submitFormData.calledWith('mock node'));
+        this.assertEqual(React.findDOMNode.callCount, 1);
+        this.assertEqual(component.submitFormData.callCount, 1);
+        this.assertTrue(React.findDOMNode.calledWith('mock form'));
+        this.assertTrue(component.submitFormData.calledWith('mock node'));
 
         delete global.FormData;
-        React.findDOMNode.restore();
-        component.submitFormData.restore();
-    });
+    },
 
-    it('should submit without FormData', () => {
-        const node = {submit: stub()};
-        const component = TestUtils.createComponent(
+    testSubmitWithoutFormData() {
+        const node = {submit: this.stub()};
+        const component = this.createComponent(
             <AjaxForm />
         );
 
         component.refs = {form: 'mock form'};
-        stub(React, 'findDOMNode', () => node);
+        this.stub(React, 'findDOMNode', () => node);
 
         component.submit();
-        assert.equal(React.findDOMNode.callCount, 1);
-        assert.equal(node.submit.callCount, 1);
-        assert.isTrue(React.findDOMNode.calledWith('mock form'));
+        this.assertEqual(React.findDOMNode.callCount, 1);
+        this.assertEqual(node.submit.callCount, 1);
+        this.assertTrue(React.findDOMNode.calledWith('mock form'));
+    },
 
-        React.findDOMNode.restore();
-    });
-
-    it('should submit form data', () => {
+    testSubmitData() {
         const form = {action: '/login/'};
-        const component = TestUtils.createComponent(
+        const component = this.createComponent(
             <AjaxForm />
         );
 
         global.FormData = function() {};
-        stub(request, 'post');
+        this.stub(request, 'post');
 
         component.submitFormData(form);
-        assert.equal(request.post.callCount, 1);
-        assert.isTrue(request.post.calledWith(
+        this.assertEqual(request.post.callCount, 1);
+        this.assertTrue(request.post.calledWith(
             '/login/',
             new global.FormData(form),
             component.onResponse
         ));
 
         delete global.FormData;
-        request.post.restore();
-    });
+    }
 });
