c06dc4b53ac5b05a028fcddf491cabf5379af056
Jeff McRiffey <jeff.mcriffey@ambition.com>
2015-08-12 15:34:38 -0400

    0.4.31

--- dist/react-ui.js
+++ dist/react-ui.js
@@ -14,7 +14,7 @@ global.ReactUI = _src2['default'];
 },{"./src":21}],2:[function(require,module,exports){
 module.exports={
   "name": "react-ui",
-  "version": "0.4.30",
+  "version": "0.4.31",
   "author": "Ambition Team",
   "license": "MIT",
   "description": "A collection of components for React.",
