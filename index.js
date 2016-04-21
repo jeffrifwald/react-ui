3d9bbb953d1096bf85324d814f0e0bf1236c1236
Jeff McRiffey <jmcriffey@gmail.com>
2016-04-21 15:44:14 -0400

    updated dependencies

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 module.exports = {
+    DatePicker: require('react-ui-component-date-picker'),
+    FileInput: require('react-ui-component-file-input'),
     Grid: require('react-ui-component-grid')
 };
